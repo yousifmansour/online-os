6e4a32780e154141b3c7f525dc33b98ef1e1d0fe
Yousif <yousifyacoub1997@gmail.com>
2019-02-13 12:35:23 +0200

    add animations to apps

--- src/applications/components/Calculator.js
+++ src/applications/components/Calculator.js
@@ -9,23 +9,25 @@ const Calculator = ({
     setSecondNumber,
     result
 }) => (
-    <form onSubmit={handleSubmit} className='calculator-container'>
-        <h2>Calculator that only adds</h2>
-        <input
-            type='number'
-            name='firstNumber'
-            value={firstNumber}
-            onChange={(e) => setFirstNumber(e.target.value)}></input>
-        <input
-            type='number'
-            name='secondNumber'
-            value={secondNumber}
-            onChange={(e) => setSecondNumber(e.target.value)}></input>
-        <button>get result</button>
-        <h3 style={{
-            textAlign: 'center'
-        }}>{result}</h3>
-    </form>
+    <div className='calculator-container'>
+        <form onSubmit={handleSubmit}>
+            <h2>Calculator that only adds</h2>
+            <input
+                type='number'
+                name='firstNumber'
+                value={firstNumber}
+                onChange={(e) => setFirstNumber(e.target.value)}></input>
+            <input
+                type='number'
+                name='secondNumber'
+                value={secondNumber}
+                onChange={(e) => setSecondNumber(e.target.value)}></input>
+            <button>get result</button>
+            <h3 style={{
+                textAlign: 'center'
+            }}>{result}</h3>
+        </form>
+    </div>
 );
 
 export default Calculator;
