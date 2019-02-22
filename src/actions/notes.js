08f840424060ce28090b1d32a6c10f56e0dc402d
Yousif <yousifyacoub1997@gmail.com>
2019-02-22 12:53:49 +0200

    fix stupid url related bug

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -11,8 +11,8 @@ export function createNote() {
 export function loadNotesFromDB() {
     // this needs to load notes (if any) and nextID and populate the state
     return (dispatch) => {
-        let url = 'http://localhost:5000';
-        // let url = 'https://www.yousifmansour.space/api/online-os';
+        // let url = 'http://localhost:5000';
+        let url = 'https://www.yousifmansour.space/api/online-os';
         fetch(url + '/notes').then((data) => {
             return data.json();
         }).then((data) => {
