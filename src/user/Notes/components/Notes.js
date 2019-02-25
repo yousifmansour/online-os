9a5e6490a9f19eb17b479b7e231bbc34d0f5d983
Yousif <yousifyacoub1997@gmail.com>
2019-02-25 11:53:01 +0200

    improve note app styling and fix a bug with ordering of note currently being edited

--- src/user/Notes/components/Notes.js
+++ src/user/Notes/components/Notes.js
@@ -4,20 +4,31 @@ import 'user/Notes/components/Notes.css';
 const Notes = ({notes, setNote, createNote}) => {
     let listOfNotes;
     if (notes) {
-        listOfNotes = notes.map((note, i) => (
+        listOfNotes = notes.sort((a, b) => a.id - b.id).map((note, i) => (
             <textarea
+                className='note'
                 key={i}
                 value={note.text}
                 onChange={(e) => setNote({id: note.id, text: e.target.value})}></textarea>
         ));
     }
     return (
-        <div className='notes-container'>
+        <div className='notes-component'>
             <h2>Notes</h2>
+
+            <button className='new-note' onClick={createNote}>+</button>
+
             {notes
                 ? listOfNotes
                 : null}
-            <button onClick={createNote}>New note</button>
+
+            <div id="myModal" className="modal">
+                <div className="modal-content">
+                    <span className="close">&times;</span>
+                    <p>Some text in the Modal..</p>
+                </div>
+            </div>
+
         </div>
     );
 };
