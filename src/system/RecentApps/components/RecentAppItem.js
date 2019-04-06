8d29e7bdfe7d6ccd1a9d6e833485133b5a039d2b
Yousif <yousifyacoub1997@gmail.com>
2019-04-06 13:37:15 +0300

    fix horizontal scrolling for recent apps

--- src/system/RecentApps/components/RecentAppItem.js
+++ src/system/RecentApps/components/RecentAppItem.js
@@ -10,30 +10,49 @@ class RecentAppItem extends React.Component {
             x: 0,
             y: 0,
             axis: 'x',
-            mode: null
+            mode: null,
+            disabled: false
         };
     }
 
     handleDrag = (e, position) => {
         if (this.state.mode === null) {
+            // not currently scorlling
             if (Math.abs(position.deltaX) > 2 * Math.abs(position.deltaY)) 
                 this.setState({mode: 'horizontal'});
             else if (Math.abs(position.deltaY) > 2 * Math.abs(position.deltaX)) 
                 this.setState({mode: 'vertical'});
             }
-        
         if (this.state.mode === 'vertical') {
             this.setState({
                 x: 0,
-                axis: 'y'
-            }, () => document.querySelector('.viewport').scrollTop -= position.y);
+                axis: 'none'
+            }, () => this.scrollViewport(position.y));
+
         } else if (this.state.mode === 'horizontal') 
             this.setState({x: position.x});
         }
     
+    scrollViewport = (amount) => {
+        let currentPosition = document
+            .querySelector('.viewport')
+            .scrollTop;
+
+        // console.log(currentPosition - amount);
+        // if (currentPosition - amount <= 0) {
+        //     document
+        //         .querySelector('.viewport')
+        //         .scrollTop = 0;
+        // } else 
+            document
+                .querySelector('.viewport')
+                .scrollTop -= amount;
+        }
+    
     handleStop = (e, position) => {
+        // this.setState({disabled: false});
         this.setState({mode: null});
-        if ((position.x > 0 && position.x > window.innerWidth / 2) || (position.x < 0 && position.x < -window.innerWidth / 2)) {
+        if ((position.x > 0 && position.x > window.innerWidth / 3) || (position.x < 0 && position.x < -window.innerWidth / 3)) {
             this.setState({
                 x: position.x,
                 axis: 'x'
@@ -51,6 +70,8 @@ class RecentAppItem extends React.Component {
     render() {
         return (
             <Draggable
+                // bounds='parent'
+                
                 axis={this.state.axis}
                 onDrag={this.handleDrag}
                 onStop={this.handleStop}
@@ -61,7 +82,10 @@ class RecentAppItem extends React.Component {
                 <div className='recent-app-item'>
                     <NavLink exact to={'/' + this.props.app} activeClassName="active">
                         <h2>
-                            {this.props.app.replace('-', ' ')}
+                            {this
+                                .props
+                                .app
+                                .replace('-', ' ')}
                         </h2>
                     </NavLink>
                     <button className='close-app-button' onClick={this.props.closeApp}>X</button>
