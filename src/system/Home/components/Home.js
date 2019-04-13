4cb5f3428ed5509218d81dcd676ba8ed26829385
Yousif <yousifyacoub1997@gmail.com>
2019-04-13 11:50:35 +0300

    fix animations so that the wallpaper is not animated because it was getting bad performence

--- src/system/Home/components/Home.js
+++ src/system/Home/components/Home.js
@@ -4,38 +4,40 @@ import TimeAndDateContainer from 'system/TimeAndDate/containers/TimeAndDateConta
 import 'system/Home/components/Home.css';
 
 const Home = (props) => (
-    <div className='list-container'>
-        <TimeAndDateContainer/>
-        <div className='links'>
-            <NavLink exact to="/calculator" activeClassName="active">
-                <div className='icon'>
+    <div className='home'>
+        <div className='list-container'>
+            <TimeAndDateContainer/>
+            <div className='links'>
+                <NavLink exact to="/calculator" activeClassName="active">
+                    <div className='icon'>
 
-                    <img
-                        width='50px'
-                        src='http://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/calculator-icon.png'/>
+                        <img
+                            width='50px'
+                            src='http://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/calculator-icon.png'/>
 
-                    Calculator
-                </div>
-            </NavLink>
+                        Calculator
+                    </div>
+                </NavLink>
 
-            <NavLink exact to="/notes" activeClassName="active">
-                <div className='
+                <NavLink exact to="/notes" activeClassName="active">
+                    <div className='
                         icon'>
-                    <img
-                        width='50px'
-                        src='http://icons.iconarchive.com/icons/alienvalley/osx-dock-replacement/512/notes-icon.png'/>
-                    Notes
-                </div>
-            </NavLink>
+                        <img
+                            width='50px'
+                            src='http://icons.iconarchive.com/icons/alienvalley/osx-dock-replacement/512/notes-icon.png'/>
+                        Notes
+                    </div>
+                </NavLink>
 
-            <NavLink exact to="/files" activeClassName="active">
-                <div className='icon'>
-                    <img
-                        width='50px'
-                        src='http://icons.iconarchive.com/icons/dtafalonso/yosemite-flat/512/Folder-icon.png'/>
-                    Files
-                </div>
-            </NavLink>
+                <NavLink exact to="/files" activeClassName="active">
+                    <div className='icon'>
+                        <img
+                            width='50px'
+                            src='http://icons.iconarchive.com/icons/dtafalonso/yosemite-flat/512/Folder-icon.png'/>
+                        Files
+                    </div>
+                </NavLink>
+            </div>
         </div>
     </div>
 );
